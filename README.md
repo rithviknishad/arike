93b3b890494d1cbe619d6c008cb333bdd9421c12
rithviknishad <rithvikn2001@gmail.com>
2022-02-22 07:37:28 +0530

    creates `arike_app` app

---
+++ README.md
@@ -0,0 +1,3 @@
+### Features to add
+
+- [ ] [django.contrib.humanize](https://docs.djangoproject.com/en/4.0/ref/templates/language/#:~:text=django.contrib.humanize)
---
+++ arike_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArikeAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'arike_app'
