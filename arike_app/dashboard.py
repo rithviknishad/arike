bd54d3bfd441312717d63cf35c1a31988b878d8e
rithviknishad <rithvikn2001@gmail.com>
2022-02-26 14:50:51 +0530

    simplify dashboard logic

--- arike_app/dashboard.py
+++ arike_app/dashboard.py
@@ -1,25 +1,18 @@
 from dataclasses import dataclass
-from typing import List, Optional
 
 
 @dataclass
-class __DashboardTab:
+class DashboardTab:
     name: str
     url: str
-    selected: bool
 
 
-DASHBOARD_PAGES: List[__DashboardTab] = []
-
-
-def register_dashboard_tab(name: str, url: str):
-    DASHBOARD_PAGES.append(__DashboardTab(name, url, False))
-
-
-class MyTabs:
-    Home = "Home", "/home"
-    Users = "Users", "/users"
-
-
-for tab in [x for x in dir(MyTabs) if not x.startswith("__")]:
-    register_dashboard_tab(*getattr(MyTabs, tab))
+DASHBOARD_PAGES = [
+    DashboardTab(*tab_details)
+    for tab_details in [
+        ("Home", "/home"),
+        ("Users", "/users"),
+        ("Facility", "/facilities"),
+        ("Patients", "/patients"),
+    ]
+]
