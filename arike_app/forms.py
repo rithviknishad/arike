dd7dd9b0c4ed4eb956940b88e9b1b77ce0a6358f
rithviknishad <rithvikn2001@gmail.com>
2022-02-27 11:28:48 +0530

    clean

--- arike_app/forms.py
+++ arike_app/forms.py
@@ -2,23 +2,13 @@ from django.contrib.auth import forms
 from arike_app.models import *
 
 
-class UserCreationForm(forms.UserCreationForm):
-    class Meta:
-        model = User
-        fields = [
-            "username",
-            "password1",
-            "password2",
-            "first_name",
-            "last_name",
-            "email",
-            "phone",
-            "facility",
-            "role",
-        ]
-
-
 class UserChangeForm(forms.UserChangeForm):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email", "phone", "facility", "role"]
+
+
+class UserCreationForm(forms.UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2", *UserChangeForm.Meta.fields]
