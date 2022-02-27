84c46819104fe467aca8ae3727dfa2f8b08be237
rithviknishad <rithvikn2001@gmail.com>
2022-02-27 09:21:25 +0530

    improve reusability of views

--- arike_app/views.py
+++ arike_app/views.py
@@ -13,12 +13,39 @@ from arike_app.models import *
 from arike_app.dashboard import DASHBOARD_PAGES
 
 
-class DashboardViewMixin(LoginRequiredMixin, ContextMixin, View):
+class DashboardViewMixin(LoginRequiredMixin, ContextMixin):
+
+    name = None
+    view_type = None
+
+    def __init__(self) -> None:
+        self.template_name = f"dashboard/{self.name}/{self.view_type}.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # TODO: show tabs based on permissions only
         context["dashboard_tabs"] = DASHBOARD_PAGES
         return context
+
+
+class CustomListView(DashboardViewMixin, ListView):
+    view_type = "list"
+
+
+class CustomDetailView(DashboardViewMixin, DetailView):
+    view_type = "details"
+
+
+class CustomUpdateView(DashboardViewMixin, UpdateView):
+    view_type = "edit"
+
+
+class CustomDeleteView(DashboardViewMixin, DeleteView):
+    view_type = "delete"
+
+
+class CustomCreateView(DashboardViewMixin, CreateView):
+    view_type = "create"
 
 
 class LoginForm(AuthenticationForm):
@@ -58,7 +85,12 @@ class ProfileUpdateView(DashboardViewMixin, UpdateView):
     template_name = "dashboard/profile.html"
 
 
-class CreateUserView(DashboardViewMixin, CreateView):
+class UsersViewMixin:
+    model = User
+    name = "users"
+
+
+class CreateUserView(UsersViewMixin, CustomCreateView):
     class UserCreationForm(UserCreationForm):
         class Meta:
             model = User
@@ -74,35 +106,27 @@ class CreateUserView(DashboardViewMixin, CreateView):
                 "role",
             ]
 
-    template_name = "dashboard/users/create.html"
     form_class = UserCreationForm
     success_url = "../"
 
-    def get_success_url(self) -> str:
-        return super().get_success_url()
 
-
-class ListUsersView(DashboardViewMixin, ListView):
-    template_name = "dashboard/users/list.html"
-
+class ListUsersView(UsersViewMixin, CustomListView):
     def get_queryset(self):
         # TODO: show only current district users.
         return User.objects.all()
 
 
-class UserDetailsView(DashboardViewMixin, DetailView):
+class UserDetailsView(UsersViewMixin, CustomDetailView):
     model = User
-    template_name = "dashboard/users/details.html"
     context_object_name = "object"  # Voluntarily overriding as it'll conflict with `user` in context
 
 
-class UserDeleteView(DashboardViewMixin, DeleteView):
+class UserDeleteView(UsersViewMixin, CustomDeleteView):
     model = User
-    template_name = "dashboard/users/delete.html"
     success_url = "/users"
 
 
-class UserEditView(DashboardViewMixin, UpdateView):
+class UserEditView(UsersViewMixin, CustomUpdateView):
     class UserChangeForm(UserChangeForm):
         class Meta:
             model = User
@@ -111,4 +135,3 @@ class UserEditView(DashboardViewMixin, UpdateView):
     model = User
     form_class = UserChangeForm
     success_url = "../"
-    template_name = "dashboard/users/edit.html"
