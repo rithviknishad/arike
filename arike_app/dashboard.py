946507fc58b02cdff8c5e76c8ebfedef40aca3ca
rithviknishad <rithvikn2001@gmail.com>
2022-02-28 17:22:15 +0530

    adds trailing slash to dashboard page urls

--- arike_app/dashboard.py
+++ arike_app/dashboard.py
@@ -10,12 +10,12 @@ class DashboardTab:
 DASHBOARD_PAGES = [
     DashboardTab(*tab_details)
     for tab_details in [
-        ("Home", "/home"),
-        ("Users", "/users"),
-        ("Facilities", "/facilities"),
-        ("Patients", "/patients"),
-        ("LSG Bodies", "/lsg-bodies"),
-        ("Wards", "/wards"),
-        ("Profile", "/profile"),
+        ("Home", "/home/"),
+        ("Users", "/users/"),
+        ("Facilities", "/facilities/"),
+        ("Patients", "/patients/"),
+        ("LSG Bodies", "/lsg-bodies/"),
+        ("Wards", "/wards/"),
+        ("Profile", "/profile/"),
     ]
 ]
