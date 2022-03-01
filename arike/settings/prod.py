2aee1113d571faa80b7a7ca709f150061a133298
rithviknishad <rithvikn2001@gmail.com>
2022-03-01 22:48:55 +0530

    updates settings

--- arike/settings/prod.py
+++ arike/settings/prod.py
@@ -10,3 +10,14 @@ ALLOWED_HOSTS = ["rithviknishad-arike.herokuapp.com"]
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
+DEFAULT_FROM_EMAIL = env(
+    "DJANGO_DEFAULT_FROM_EMAIL",
+    default="Arike <noreply@example.com>",
+)
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#server-email
+SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
