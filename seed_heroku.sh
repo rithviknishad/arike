3af65fc6db6e19c2ea28d3c822b4b085a7960e73
rithviknishad <rithvikn2001@gmail.com>
2022-03-01 13:15:30 +0530

    updates

--- seed_heroku.sh
+++ seed_heroku.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+git push heroku main;
+heroku pg:reset DATABASE_URL -a rithviknishad-arike --confirm rithviknishad-arike;
 heroku run python manage.py migrate;
 cat fixtures/State.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
 cat fixtures/District.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
