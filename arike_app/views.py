fc57ad8d52eae66c14783dff8d7d0765037a9e11
rithviknishad <rithvikn2001@gmail.com>
2022-03-03 07:46:17 +0530

    CRUD view for Treatment

--- arike_app/views.py
+++ arike_app/views.py
@@ -292,3 +292,25 @@ class PatientDiseaseHistoryViews:
 
     class Delete(_ViewMixin, GenericModelDeleteView):
         pass
+
+
+class PatientTreatmentsViews:
+    class _ViewMixin(PatientRelatedViewMixin):
+        model = PatientDisease
+        name = "patient-treatments"
+        form_class = PatientDiseaseForm
+
+    class Create(_ViewMixin, GenericModelCreateView):
+        pass
+
+    class List(_ViewMixin, GenericModelListView):
+        pass
+
+    class Details(_ViewMixin, GenericModelDetailView):
+        pass
+
+    class Update(_ViewMixin, GenericModelUpdateView):
+        pass
+
+    class Delete(_ViewMixin, GenericModelDeleteView):
+        pass
