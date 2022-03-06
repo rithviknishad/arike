b3b9ae2c36251e81d9f2f2c0afc532a1fd236c35
rithviknishad <rithvikn2001@gmail.com>
2022-03-06 13:30:39 +0530

    updates seed program

--- seed.sh
+++ seed.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/bash
 ./manage.py migrate;
-./manage.py loaddata fixtures/State.json;
-./manage.py loaddata fixtures/District.json;
-./manage.py loaddata fixtures/LsgBody.json;
-./manage.py loaddata fixtures/Ward.json;
-./manage.py loaddata fixtures/Facility.json;
-./manage.py loaddata fixtures/User.json;
-./manage.py loaddata fixtures/Patient.json;
-./manage.py loaddata fixtures/Disease.json;
-./manage.py loaddata fixtures/CareType.json;
-./manage.py loaddata fixtures/CareSubType.json;
+./manage.py loaddata fixtures.yaml --format=yaml
--- seed_heroku.sh
+++ seed_heroku.sh
@@ -2,13 +2,4 @@
 git push heroku main;
 heroku pg:reset DATABASE_URL -a rithviknishad-arike --confirm rithviknishad-arike;
 heroku run python manage.py migrate;
-cat fixtures/State.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/District.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/LsgBody.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/Ward.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/Facility.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/User.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/Patient.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/Disease.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/CareType.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
-cat fixtures/CareSubType.json | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=json -;
+cat fixtures.yaml | heroku run --no-tty -a rithviknishad-arike -- python manage.py loaddata --format=yaml -;
