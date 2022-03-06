76cc3c860561bdcbc2f44f982d10f0a2bf437208
rithviknishad <rithvikn2001@gmail.com>
2022-03-06 11:36:24 +0530

    dynamically create visit detail and treament notes

--- arike_app/views/schedule.py
+++ arike_app/views/schedule.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from arike_app.models import Patient, VisitDetails, VisitSchedule
+from arike_app.models import Patient, TreatmentNotes, VisitDetails, VisitSchedule
 from arike_app.views.generic import (
     GenericModelCreateView,
     GenericModelDeleteView,
@@ -54,6 +54,16 @@ class VisitPatient(__ScheduleViewsMixin, GenericModelDetailView):
         # Voluntarily overriding to hide button. TODO: update template instead of context
         context["has_update_perm"] = False
         context["has_delete_perm"] = False
+        visit_details = VisitDetails.objects.filter(visit_schedule=self.object)
+        visit_details = (
+            visit_details.first()
+            if visit_details
+            else VisitDetails.objects.create(
+                visit_schedule=self.object, treatment_notes=TreatmentNotes.objects.create()
+            )
+        )
+        context["on_click_treatment_notes"] = f"location.href='../treatment_notes/{visit_details.treatment_notes.id}'"
+        context["on_click_health_information"] = f"location.href='../health_info/{visit_details.id}'"
         return context
 
 
